f762533c024843e2d903c5e8d224df22a8c5978c
H-Anna <anna.k.harsanyi@gmail.com>
2021-11-21 14:51:15 +0100

    Added a few talks. Added more animations. Changed FileReader::ReadFile. Removed talks2.json and desktop.ini.

--- src/workinstance/filereader/filereader.cpp
+++ src/workinstance/filereader/filereader.cpp
@@ -22,18 +22,60 @@ bool FileReader::HasDataFolder(QString *folderPath)
 
 bool FileReader::ReadFiles(QList<QJsonObject> *validObjects, QString *iniFile, QString *stylesheet, const QString &absolutePath)
 {
-    int validCount = 0;
+    int valid = 0;
+    int total = 0;
+    QDir dir(absolutePath);
+    QStringList JSON("*.json");
+    QStringList OTHER({"variables.ini", "default.ini", "stylesheet.qss"});
 
-    auto filesInfoList = QDir(absolutePath).entryInfoList(QDir::Files);
+    dir.setNameFilters(JSON);
+    auto jsonList = dir.entryInfoList(QDir::Files);
+    dir.setNameFilters(OTHER);
+    auto otherList = dir.entryInfoList(QDir::Files);
 
-    if (filesInfoList.isEmpty()) {
-        qDebug() << "ERROR - FileReader - No files in directory!";
+    if (jsonList.isEmpty()) {
+        qDebug() << "ERROR - FileReader - No JSON files in directory!";
         return false;
     }
 
+    total = jsonList.count();
+
     ///Read files, convert to JSON object and determine if it can be parsed
 
-    for (auto &it: filesInfoList) {
+    for(auto &it: jsonList)
+    {
+        auto file = new QFile(it.absoluteFilePath());
+        file->open(QIODevice::ReadOnly);
+        QString contents = file->readAll();
+        file->close();
+
+        QJsonObject json = QJsonDocument::fromJson(contents.toUtf8()).object();
+
+        if (json.isEmpty()) {
+            qDebug().noquote() << QString("WARNING - FileReader - File %1 has invalid JSON. "
+                                          "Unable to process. Skipping file.").arg(file->fileName());
+            continue;
+        }
+
+        QString type = json.value("type").toString();
+
+        if (type.isEmpty()) {
+            qDebug().noquote() << QString("WARNING - FileReader - File %1 has invalid JSON. "
+                        "No 'type' defined. Skipping file.").arg(file->fileName());
+            continue;
+        }
+
+        delete file;
+
+        validObjects->append(json);
+        valid++;
+    }
+
+    if (otherList.isEmpty()) {
+        qDebug() << "ERROR - FileReader - No .ini or .qss files in directory!";
+        return false;
+    }
+    for (auto &it: otherList) {
 
         if (it.fileName().endsWith(".ini")) {
 
@@ -49,38 +91,9 @@ bool FileReader::ReadFiles(QList<QJsonObject> *validObjects, QString *iniFile, Q
             delete file;
 
         }
-
-        if (it.fileName().endsWith(".json")) {
-
-            auto file = new QFile(it.absoluteFilePath());
-            file->open(QIODevice::ReadOnly);
-            QString contents = file->readAll();
-            file->close();
-
-            QJsonObject json = QJsonDocument::fromJson(contents.toUtf8()).object();
-
-            if (json.isEmpty()) {
-                qDebug().noquote() << QString("WARNING - FileReader - File %1 has invalid JSON. "
-                                              "Unable to process. Skipping file.").arg(file->fileName());
-                continue;
-            }
-
-            QString type = json.value("type").toString();
-
-            if (type.isEmpty()) {
-                qDebug().noquote() << QString("WARNING - FileReader - File %1 has invalid JSON. "
-                            "No 'type' defined. Skipping file.").arg(file->fileName());
-                continue;
-            }
-
-            delete file;
-
-            validObjects->append(json);
-            validCount++;
-        }
     }
 
-    qDebug().noquote() << QString("INFO - FileReader - Read %1 files as valid JSONs.").arg(validCount);
+    qDebug().noquote() << QString("INFO - FileReader - Read %1 valid JSONs (out of %2 total.)").arg(valid).arg(total);
 
     return true;
 }
